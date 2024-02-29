5dec365c4c764b2d64e0f4330a1255e45f1af017
kahhorjonov <kahhorjonov_codes@kahhorjonovs-MacBook-Pro.local>
2024-03-01 02:47:52 +0500

    crypto prices app done

---
+++ lib/services/networking.dart
@@ -0,0 +1,12 @@
+import 'package:http/http.dart' as http;
+
+class NetworkHelper {
+  NetworkHelper(this.url);
+
+  final String url;
+
+  Future getData() async {
+    http.Response response = await http.get(Uri.parse(url));
+    return response;
+  }
+}
